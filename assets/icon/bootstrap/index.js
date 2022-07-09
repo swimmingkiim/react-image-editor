441e77601d324a7ab0f7213ed827794f2977a760
swimmingkiim <swimmingkiim@gmail.com>
2022-07-09 17:27:45 +0900

    Updates

---
+++ assets/icon/bootstrap/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+function main() {
+  const list = fs.readdirSync(".").filter((file) => file.endsWith(".svg"));
+  let result = list.map((filename) => ({
+    type: "icon",
+    id: filename.replace(".svg", ""),
+    name: filename.replace(".svg", "").replaceAll("-", " "),
+    icon: filename
+  }));
+  fs.writeFileSync("../../../../src/config/icon.json", JSON.stringify(result));
+}
+
+main();
