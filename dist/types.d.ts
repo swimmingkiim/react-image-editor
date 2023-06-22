c7c202c5be5fcd12a47c204ea2abd05f3e2ce3f2
swimmingkiim <swimmingkiim@gmail.com>
2023-06-22 16:33:51 +0900

    Update dist with canvas

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,3 +1,30 @@
-declare const testFunc: () => void;
+import * as React from 'react';
+import { Canvas } from 'fabric';
 
-export { testFunc };
+type ReactImageEditorCanvasParams = {
+    fabricCanvasOptions: {
+        [key: string]: any;
+    };
+    enableDragAndDrop?: boolean;
+    enableSelection?: boolean;
+    enableZoom?: boolean;
+    enableHotKey?: boolean;
+};
+declare class ReactImageEditor {
+    _fabricCanvas: Canvas;
+    constructor(params: ReactImageEditorCanvasParams);
+    static REACT_IMAGE_EDITOR__CANVAS_ID: string;
+}
+
+type ReactImageEditorCanvasProps = {
+    reactImageEditor: ReactImageEditor;
+    children: React.ReactNode;
+};
+
+declare const modules: {
+    ReactImageEditorCanvasContext: React.Context<ReactImageEditor | undefined>;
+    ReactImageEditorCanvas: ({ children, ...props }: ReactImageEditorCanvasProps) => React.JSX.Element;
+    ReactImageEditor: typeof ReactImageEditor;
+};
+
+export { modules as default };
