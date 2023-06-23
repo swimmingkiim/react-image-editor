86b5bfa53fc19570c98ba4ea962e15c5e43e11a7
swimmingkiim <swimmingkiim@gmail.com>
2023-06-23 10:18:51 +0900

    Add Rect Shape with basic spec

---
+++ src/core/editableObject/shape/rect/component.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { useState, useContext, useEffect } from "react";
+
+import { ReactImageEditorCanvasContext } from "../../../canvas/component";
+import { ShapeRect, RectParams } from "./rect";
+
+export type RectProps = {
+  children?: React.ReactNode;
+} & Omit<RectParams, "editorController">;
+
+export const Rect = ({ children, ...props }: RectProps) => {
+  const reactImageEditor = useContext(ReactImageEditorCanvasContext);
+
+  const newRect = new ShapeRect({
+    ...props,
+    editorController: reactImageEditor!,
+  });
+  reactImageEditor!.addObject(newRect);
+
+  return <>{children}</>;
+};
