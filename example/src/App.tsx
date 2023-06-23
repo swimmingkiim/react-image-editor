5877e9f30e97996730e637aac050cc394a7ce4e4
swimmingkiim <swimmingkiim@gmail.com>
2023-06-23 10:19:14 +0900

    Update example with Rect

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,24 @@
-import "./App.css";
+import { useEffect } from "react";
+
 import RIE from "react-image-editor";
+
+import "./App.css";
 
 function App() {
   const editor = new RIE.ReactImageEditor({
-    fabricCanvasOptions: {},
+    fabricCanvasOptions: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
   });
+
+  useEffect(() => {
+    console.log(editor.data);
+  }, []);
 
   return (
     <RIE.ReactImageEditorCanvas reactImageEditor={editor}>
-      <></>
+      <RIE.Rect x={100} y={100} width={300} height={300} />
     </RIE.ReactImageEditorCanvas>
   );
 }
