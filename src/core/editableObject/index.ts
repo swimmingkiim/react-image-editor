83fc7236738e9bf9ac113d671d509d0eb46a5a57
swimmingkiim <swimmingkiim@gmail.com>
2023-06-23 10:19:01 +0900

    Update barrel files

---
+++ src/core/editableObject/index.ts
@@ -0,0 +1,9 @@
+import * as EditableObject from './editableObject';
+import shapeModules from './shape';
+
+const modules = {
+  ...EditableObject,
+  ...shapeModules,
+};
+
+export default modules;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import canvasModules from './core/canvas';
+import editableObjectModules from './core/editableObject';
 
 const  modules = {
   ...canvasModules,
+  ...editableObjectModules,
 };
 
 export default modules;
