db5fa3139ff9d4dfd692abde75c048f135a91076
swimmingkiim <54804279+swimmingkiim@users.noreply.github.com>
2023-06-26 20:28:05 +0900

    Add import and export (#21)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import RIE from "react-image-editor";
 
 import "./App.css";
@@ -10,9 +12,14 @@ function App() {
     },
   });
 
+  useEffect(() => {
+    const data = `{"version":"6.0.0-beta10","objects":[{"rx":0,"ry":0,"type":"Rect","version":"6.0.0-beta10","originX":"left","originY":"top","left":100,"top":100,"width":300,"height":300,"fill":"#000000","stroke":"#000000","strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0}]}`;
+    editor.import(JSON.parse(data));
+  }, []);
+
   return (
     <RIE.ReactImageEditorCanvas reactImageEditor={editor}>
-      <RIE.Rect x={100} y={100} width={300} height={300} />
+      {/* <RIE.Rect x={100} y={100} width={300} height={300} /> */}
     </RIE.ReactImageEditorCanvas>
   );
 }
