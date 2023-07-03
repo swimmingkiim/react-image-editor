fcf8092d98043da8ff893f342e4338ca6bdf0a31
swimmingkiim <swimmingkiim@gmail.com>
2023-07-03 11:07:53 +0900

    Update example code with `initialState`

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import RIE from "react-image-editor";
 
 import "./App.css";
@@ -10,12 +8,57 @@ function App() {
       width: window.innerWidth,
       height: window.innerHeight,
     },
+    initialState: [
+      new RIE.Tab({
+        name: "default",
+        initialHistory: new RIE.History({
+          initialData: new RIE.Data({
+            prevData: null,
+            snapshot: {
+              version: "6.0.0-beta10",
+              objects: [
+                {
+                  rx: 0,
+                  ry: 0,
+                  type: "Rect",
+                  version: "6.0.0-beta10",
+                  originX: "left",
+                  originY: "top",
+                  left: 100,
+                  top: 100,
+                  width: 300,
+                  height: 300,
+                  fill: "#000000",
+                  stroke: "#000000",
+                  strokeWidth: 1,
+                  strokeDashArray: null,
+                  strokeLineCap: "butt",
+                  strokeDashOffset: 0,
+                  strokeLineJoin: "miter",
+                  strokeUniform: false,
+                  strokeMiterLimit: 4,
+                  scaleX: 1,
+                  scaleY: 1,
+                  angle: 0,
+                  flipX: false,
+                  flipY: false,
+                  opacity: 1,
+                  shadow: null,
+                  visible: true,
+                  backgroundColor: "",
+                  fillRule: "nonzero",
+                  paintFirst: "fill",
+                  globalCompositeOperation: "source-over",
+                  skewX: 0,
+                  skewY: 0,
+                },
+              ],
+            },
+          }),
+        }),
+      }),
+    ],
   });
-
-  useEffect(() => {
-    const data = `{"version":"6.0.0-beta10","objects":[{"rx":0,"ry":0,"type":"Rect","version":"6.0.0-beta10","originX":"left","originY":"top","left":100,"top":100,"width":300,"height":300,"fill":"#000000","stroke":"#000000","strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0}]}`;
-    editor.import(JSON.parse(data));
-  }, []);
 
   return (
     <RIE.ReactImageEditorCanvas reactImageEditor={editor}>
