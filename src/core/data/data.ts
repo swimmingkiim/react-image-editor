b2896dd1ad3790e52288f6289a445290101379e7
swimmingkiim <swimmingkiim@gmail.com>
2023-07-03 11:05:38 +0900

    Add `Data` class

---
+++ src/core/data/data.ts
@@ -0,0 +1,40 @@
+import { Canvas } from 'fabric';
+
+export interface DataParams {
+  prevData: Data | null;
+  snapshot: ReturnType<Canvas['toJSON']>;
+}
+
+export interface DataProperties {
+  snapshot: ReturnType<Canvas['toJSON']>;
+  prevData: Data | null;
+  nextData: Data | null;
+  createdAt: Date;
+}
+
+export class Data implements DataProperties {
+
+  snapshot: ReturnType<Canvas['toJSON']>;
+  prevData: Data | null;
+  nextData: Data | null;
+  createdAt: Date;
+
+  constructor(params:DataParams) {
+    this.snapshot = params.snapshot;
+    this.prevData = params.prevData;
+    this.nextData = null;
+    this.createdAt = new Date();
+  }
+
+  static DEFAULT_SNAPSHOT = {
+    "version":"6.0.0-beta10",
+    "objects":[],
+  };
+
+  static empty(): Data {
+    return new Data({
+      prevData: null,
+      snapshot: Data.DEFAULT_SNAPSHOT,
+    });
+  }
+}
---
+++ src/core/data/index.ts
@@ -0,0 +1,7 @@
+import * as Data from "./data";
+
+const modules = {
+  ...Data,
+};
+
+export default modules;
