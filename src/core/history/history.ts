bb625bd5dcb565a3c2bb5c0461bb1dedcde6f6a7
swimmingkiim <54804279+swimmingkiim@users.noreply.github.com>
2023-07-03 11:08:36 +0900

    Add `Data`, `History`, `Tab` classes (#23)

---
+++ src/core/history/history.ts
@@ -0,0 +1,48 @@
+import { Data } from '../data/data';
+
+export interface HistoryParams {
+  initialData: Data;
+}
+
+export interface HistoryProperties {
+  pointer: Data;
+}
+
+export interface HistoryMethods {
+  goPrev(): void;
+  goNext(): void;
+  push(data: Data): void;
+}
+
+export class History implements HistoryProperties, HistoryMethods {
+
+  pointer: Data;
+
+  constructor(params:HistoryParams) {
+    this.pointer = params.initialData;
+  } 
+
+  static empty(): History {
+    return new History({
+      initialData: Data.empty(),
+    });
+  }
+
+  goPrev(): void {
+    const prevData = this.pointer.prevData;
+    if (prevData) {
+      this.pointer = prevData;
+    }
+  }
+  goNext(): void {
+    const nextData = this.pointer.nextData;
+    if (nextData) {
+      this.pointer = nextData;
+    }
+  }
+
+  push(data: Data): void {
+    this.pointer.nextData = data;
+    this.pointer = this.pointer.nextData;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import canvasModules from './core/canvas';
+import dataModules from './core/data';
+import historyModules from './core/history';
+import tabModules from './core/tab';
 import editableObjectModules from './core/editableObject';
 
 const  modules = {
   ...canvasModules,
+  ...dataModules,
+  ...historyModules,
+  ...tabModules,
   ...editableObjectModules,
 };
 
