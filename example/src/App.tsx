59cb131d0df6f6792fa2c360350e5f37ee7d3c35
swimmingkiim <swimmingkiim@gmail.com>
2024-02-25 17:59:14 +0900

    Comment out dummy initial data

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,51 +11,52 @@ function App() {
     initialState: [
       new RIE.Tab({
         name: "default",
-        initialHistory: new RIE.History({
-          initialData: new RIE.Data({
-            prevData: null,
-            snapshot: {
-              version: "6.0.0-beta10",
-              objects: [
-                {
-                  rx: 0,
-                  ry: 0,
-                  type: "Rect",
-                  version: "6.0.0-beta10",
-                  originX: "left",
-                  originY: "top",
-                  left: 100,
-                  top: 100,
-                  width: 300,
-                  height: 300,
-                  fill: "#000000",
-                  stroke: "#000000",
-                  strokeWidth: 1,
-                  strokeDashArray: null,
-                  strokeLineCap: "butt",
-                  strokeDashOffset: 0,
-                  strokeLineJoin: "miter",
-                  strokeUniform: false,
-                  strokeMiterLimit: 4,
-                  scaleX: 1,
-                  scaleY: 1,
-                  angle: 0,
-                  flipX: false,
-                  flipY: false,
-                  opacity: 1,
-                  shadow: null,
-                  visible: true,
-                  backgroundColor: "",
-                  fillRule: "nonzero",
-                  paintFirst: "fill",
-                  globalCompositeOperation: "source-over",
-                  skewX: 0,
-                  skewY: 0,
-                },
-              ],
-            },
-          }),
-        }),
+        initialHistory: RIE.History.empty(),
+        /* initialHistory: new RIE.History({ */
+        /*   initialData: new RIE.Data({ */
+        /*     prevData: null, */
+        /*     snapshot: { */
+        /*       version: "6.0.0-beta10", */
+        /*       objects: [ */
+        /*         { */
+        /*           rx: 0, */
+        /*           ry: 0, */
+        /*           type: "Rect", */
+        /*           version: "6.0.0-beta10", */
+        /*           originX: "left", */
+        /*           originY: "top", */
+        /*           left: 100, */
+        /*           top: 100, */
+        /*           width: 300, */
+        /*           height: 300, */
+        /*           fill: "#000000", */
+        /*           stroke: "#000000", */
+        /*           strokeWidth: 1, */
+        /*           strokeDashArray: null, */
+        /*           strokeLineCap: "butt", */
+        /*           strokeDashOffset: 0, */
+        /*           strokeLineJoin: "miter", */
+        /*           strokeUniform: false, */
+        /*           strokeMiterLimit: 4, */
+        /*           scaleX: 1, */
+        /*           scaleY: 1, */
+        /*           angle: 0, */
+        /*           flipX: false, */
+        /*           flipY: false, */
+        /*           opacity: 1, */
+        /*           shadow: null, */
+        /*           visible: true, */
+        /*           backgroundColor: "", */
+        /*           fillRule: "nonzero", */
+        /*           paintFirst: "fill", */
+        /*           globalCompositeOperation: "source-over", */
+        /*           skewX: 0, */
+        /*           skewY: 0, */
+        /*         }, */
+        /*       ], */
+        /*     }, */
+        /*   }), */
+        /* }), */
       }),
     ],
   });
