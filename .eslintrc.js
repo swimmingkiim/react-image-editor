ef2dbf49818db7e01a6f32ee0071062f02b0d234
swimmingkiim <swimmingkiim@gmail.com>
2024-04-01 14:54:19 +0900

    Add prettier related settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: [
-    'example/**/*'
-  ],
-  overrides: [
-  ],
+  ignorePatterns: ["example/**/*"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ["react"],
+  rules: {},
+};
